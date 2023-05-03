5d64e5c23f2d51022c034ff37b3ea7cb91153843
alucinanteccl <l.conde@udc.gal>
2023-05-03 18:52:30 +0200

    Ampliando código

--- create_tables_ttbda.sql
+++ create_tables_ttbda.sql
@@ -1,8 +1,8 @@
- /*CREACIÓN DE LA BASE DE DATOS 'PEDIDOS' */
+ /*CREACIÓN DE LA BASE DE DATOS 'Protectora' */
 
 drop database if exists ttbda;
 drop table if exists apadriñante;
-drop table if exists gato;
+drop table if exists cuota;
 drop table if exists can;
 
 create database ttbda;
@@ -11,19 +11,19 @@ CREATE TABLE can(
 codchip bigint NOT NULL,
 nome varchar(30) NOT NULL,
 observacions varchar(4096) NULL,
+DNI_apadriñante varchar(9) NULL,
 CONSTRAINT PK_can PRIMARY KEY (codchip));
 
-CREATE TABLE gato(
-codchip bigint NOT NULL,
+CREATE TABLE cuota(
+codcuota int NOT NULL,
 nome varchar(30) NOT NULL,
-observacions varchar(4096) NULL,
-CONSTRAINT PK_gato PRIMARY KEY (codchip));
+valor numeric(5,2) constraint c_cuota_valor check (valor > 0),
+CONSTRAINT PK_cuota PRIMARY KEY (codcuota));
 
 CREATE TABLE apadriñante(
-DNI bigint NOT NULL,
+DNI varchar(9) NOT NULL,
 nome varchar(30) NOT NULL,
 apelido1 varchar(30) NOT NULL,
 apelido2 varchar(30) NOT NULL,
-cuota numeric(5,2) constraint c_apadriñante_cuota check (cuota > 0),
-codchip bigint NOT NULL,
+codcuota int NULL,
 CONSTRAINT PK_apadriñante PRIMARY KEY (DNI));
--- ttbda.py
+++ ttbda.py
@@ -29,9 +29,9 @@ def disconnect_db(conn):
 
 
 ## ------------------------------------------------------------
-def add_dog(conn):
+def add_can(conn):
     """
-        Crea e engade una nova Conta á BD cos datos introducidos polo
+        Crea e engade un novo Can á BD da protectora cos datos introducidos polo
         usuario
     """
     conn.isolation_level = psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED
@@ -42,27 +42,101 @@ def add_dog(conn):
 
     nome = input ("Introduce o nome do novo can:")
     observacions = input ("Introduce algunhas observacións necesarias:")
+    dni = input ("Introduce o dni do seu apadriñante se o ten:")
 
     sql= """
-        insert into can (codchip,nome,observacions) 
-        VALUES  (%(chip)s,%(nome)s,%(observacions)s)
+        insert into can (codchip,nome,observacions,DNI_apadriñante) 
+        VALUES  (%(chip)s,%(nome)s,%(observacions)s,%(DNI_apadriñante)s)
     """
     
     with conn.cursor() as cur:                                      
         try:                     
-            cur.execute(sql,{'chip':chip,'nome':nome,'observacions':observacions})
+            cur.execute(sql,{'chip':chip,'nome':nome,'observacions':observacions,'DNI_apadriñante':dni})
             conn.commit()
-            print('Can con chip:',chip,', nome:', nome, ' e observacións:', observacions, ' engadida correctamente')
+            print('Can con chip:',chip,', nome:', nome, ', observacións:', observacions, ' e DNI do apadriñante:', nome, ' engadida correctamente')
             
         except psycopg2.Error as e:
             if e.pgcode==psycopg2.errorcodes.NOT_NULL_VIOLATION:
                 print("Error: non pode haber campos sen valor")
             elif e.pgcode==psycopg2.errorcodes.UNIQUE_VIOLATION:
-                print("Error: xa existe unha conta co campo DNI [",dni,"]")
-            elif e.pgcode==psycopg2.errorcodes.NUMERIC_VALUE_OUT_OF_RANGE:
-                print("Error: o valor do saldo da nova introducido non é válido")
+                print("Error: xa existe un can co número de chip asociado:",chip,"")
             elif e.pgcode==psycopg2.errorcodes.STRING_DATA_RIGHT_TRUNCATION:
-                print("Error: o DNI introducido está fora do límite de caracteres")
+                print("Error: os valores introducidos están fora do límite de caracteres")
+            elif e.pgcode==psycopg2.errorcodes.CHECK_VIOLATION:
+                print("Error: o valor dos datos introducidos debe ser válido")
+            else:
+                print(f'Error generico: {e.pgcode}: {e.pgerror}')
+            conn.rollback()
+
+## ------------------------------------------------------------
+def add_apadriñante(conn):
+    """
+        Crea e engade un novo Can á BD da protectora cos datos introducidos polo
+        usuario
+    """
+    conn.isolation_level = psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED
+
+    dni = input ("Introduce o dni do novo apadriñante: ")
+    nome = input ("Introduce o nome do novo apadriñante: ")
+    apelido1 = input ("Introduce o primeiro apelido do novo apadriñante: ")
+    apelido2 = input ("Introduce o segundo apelido do novo apadriñante: ")
+    codcuota = input ("Introduce o código da cuota a pagar, para o(s) can(s) apadriñado(s) do novo apadriñante: ")
+
+    sql= """
+        insert into apadriñante (DNI,nome,apelido1,apelido2,codcuota) 
+        VALUES  (%(DNI)s,%(nome)s,%(apelido1)s,%(apelido2)s,%(codcuota)s)
+    """
+    
+    with conn.cursor() as cur:                                      
+        try:                     
+            cur.execute(sql,{'DNI':dni,'nome':nome,'apelido1':apelido1,'apelido2':apelido2,'codcuota':codcuota})
+            conn.commit()
+            print('Apadriñante con DNI:',dni,', nome:', nome,', apelidos', apelido1,'', apelido2,' e código de cuota ',codcuota,'engadida correctamente')
+            
+        except psycopg2.Error as e:
+            if e.pgcode==psycopg2.errorcodes.NOT_NULL_VIOLATION:
+                print("Error: non pode haber campos sen valor")
+            elif e.pgcode==psycopg2.errorcodes.UNIQUE_VIOLATION:
+                print("Error: xa existe un apadriñante co DNI asociado:",dni,"")
+            elif e.pgcode==psycopg2.errorcodes.STRING_DATA_RIGHT_TRUNCATION:
+                print("Error: os valores introducidos están fora do límite de caracteres")
+            elif e.pgcode==psycopg2.errorcodes.CHECK_VIOLATION:
+                print("Error: o valor dos datos introducidos debe ser válido")
+            else:
+                print(f'Error generico: {e.pgcode}: {e.pgerror}')
+            conn.rollback()
+
+## ------------------------------------------------------------
+def add_cuota(conn):
+    """
+        Crea e engade un novo Can á BD da protectora cos datos introducidos polo
+        usuario
+    """
+    conn.isolation_level = psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED
+
+    codcuota = input("Introduce o Código da nova cuota: ")
+    nome = input ("Introduce o nome da nova cuota: ")
+    valor = input ("Introduce O valor en euros da nova cuota: ")
+
+
+    sql= """
+        insert into cuota (codcuota,nome,valor) 
+        VALUES  (%(codcuota)s,%(nome)s,%(valor)s)
+    """
+    
+    with conn.cursor() as cur:                                      
+        try:                     
+            cur.execute(sql,{'codcuota':codcuota,'nome':nome,'valor':valor})
+            conn.commit()
+            print('Cuota con código:',codcuota,', nome:', nome, ' e valor:',valor, '€   engadida correctamente')
+            
+        except psycopg2.Error as e:
+            if e.pgcode==psycopg2.errorcodes.NOT_NULL_VIOLATION:
+                print("Error: non pode haber campos sen valor")
+            elif e.pgcode==psycopg2.errorcodes.UNIQUE_VIOLATION:
+                print("Error: xa existe un can co número de chip asociado:",codcuota,"")
+            elif e.pgcode==psycopg2.errorcodes.STRING_DATA_RIGHT_TRUNCATION:
+                print("Error: os valores introducidos están fora do límite de caracteres")
             elif e.pgcode==psycopg2.errorcodes.CHECK_VIOLATION:
                 print("Error: o valor dos datos introducidos debe ser válido")
             else:
@@ -74,10 +148,10 @@ def add_dog(conn):
 ##-------------------------------------------------------------
 def menu(conn):
     MENU_TEXT = """
-      -- MENÚ --
-1 - ...
-2 - ...
-3 - ...
+      -- MENÚ DA PROTECTORA --
+1 - Engadir can
+2 - Engadir apadriñante
+3 - Engadir cuota
 4 - ...
 5 - ...
 6 - ...
@@ -91,11 +165,11 @@ q - Saír
         if tecla == 'q':
             break
         elif tecla == '1':
-            create_tables(conn)
+            add_can(conn)
         elif tecla == '2':
-            add_dog(conn)
-        #elif tecla == '3':
-            ##insert_row(conn)
+            add_apadriñante(conn)
+        elif tecla == '3':
+            add_cuota(conn)
         #elif tecla == '4':
             #delete_row(conn)
         #elif tecla == '5':
